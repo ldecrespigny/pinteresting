bdda592ce6a027b114c1ec8ef1506738d03866f7
Lachlan de Crespigny <ldecrespigny@gmail.com>
2014-01-28 15:55:32 -0500

    hashed home.html.erb and put signup and login in header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125203817) do
+ActiveRecord::Schema.define(version: 20140128202117) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 20140125203817) do
     t.datetime "image_updated_at"
     t.string   "pricing"
     t.decimal  "user_review_summary"
+    t.string   "blurimage_file_name"
+    t.string   "blurimage_content_type"
+    t.integer  "blurimage_file_size"
+    t.datetime "blurimage_updated_at"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
